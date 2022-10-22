993a78ddc1b8d709041ceed205c476204eeabc8f
And_i <And_i@C18H27NO3>
2022-10-22 23:46:41 +0800

    first commit

---
+++ src/pbo/Soal2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo;
+
+/**
+ *
+ * @author And_i
+ */
+public class Soal2 {
+     public static void main(String[] args){
+String nama = "Muhammad Andi Salam", Program_Studi = "Teknologi Informasi",
+        Nama_Kampus_Dan_Fakultas ="HST ITS / Teknologi Informasi";
+System.out.println("Nama                 : "+nama);
+System.out.println("Program Studi        : "+Program_Studi);
+System.out.println("Nama Kampus/Fakultas : "+Nama_Kampus_Dan_Fakultas);
+}
+}
