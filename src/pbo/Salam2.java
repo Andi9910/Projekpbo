133d429a5bb9b74c4209e8a0efe59067cf8edca3
Andi9910 <110269848+Andi9910@users.noreply.github.com>
2022-11-02 22:51:28 +0800

    Update Salam2.java

--- src/pbo/Salam2.java
+++ src/pbo/Salam2.java
@@ -11,11 +11,11 @@ package pbo;
 public class Salam2 {
 public static void main(String[] args) {
 // Buat karakter 
-char c1 = 'S';
-char c2 = 'A'; 
-char c3 = 'L'; 
-char c4 = 'A'; 
-char c5 = 'M';
+String c1 = 'S';
+String c2 = 'A'; 
+String c3 = 'L'; 
+String c4 = 'A'; 
+String c5 = 'M';
 // Print 
 System.out.println(c1+c2+c3+c4+c5); 
 System.out.println(c5+c4+c3+c2+c1);
